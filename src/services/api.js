96528a05969847f62a0b3d09e40d78002142a1d9
Kedhar <satyasaikedhar2002@gmail.com>
2025-08-03 23:00:04 +0530

    Fix: use production backend API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://127.0.0.1:8000/api';
+const BASE_URL = 'https://health-backend.onrender.com/api';
+
 
 export const getWorkouts = () => axios.get(`${API_BASE}/workouts/`);
 export const addWorkout = (data) => axios.post(`${API_BASE}/workouts/`, data);
